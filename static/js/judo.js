e4a05d738186fdae2ce51b1ee881e8c863e4350d
Noah Hüsser <yatekii@yatekii.ch>
2019-10-20 00:16:29 +0200

    Mui changerino

---
+++ static/js/judo.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('dojo-events-judo');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        plugins: [ 'dayGrid', 'list', 'googleCalendar' ],
+        header: {
+            left: '',
+            center: '',
+            right: ''
+        },
+        defaultView: 'listYear',
+
+        locale: 'de',
+
+        displayEventTime: true,
+
+        // THIS KEY WON'T WORK IN PRODUCTION!!!
+        // To make your own Google API key, follow the directions here:
+        // http://fullcalendar.io/docs/google_calendar/
+        googleCalendarApiKey: 'AIzaSyDcnW6WejpTOCffshGDDb4neIrXVUA1EAE',
+        // http://www.google.com/calendar/embed?showTitle=0&showPrint=0&showTz=0&mode=WEEK&height=1150&wkst=2&hl=de&bgcolor=%23ffffff&src=jjjc.aarau%40gmail.com&color=%23AB8B00&src=eadllljae75tt86ddm6kvj3htk%40group.calendar.google.com&color=%232952A3&src=g3hev6racu6ngnejarktaeo7os%40group.calendar.google.com&color=%23A32929&ctz=Europe%2FZurich
+
+        eventSources: [{
+            googleCalendarId: 'utsa4grrafus1nh5o20v03pjsk@group.calendar.google.com',
+        }, {
+            googleCalendarId: 'bodkodg85clmttg0phvte9ppkc@group.calendar.google.com',
+        }],
+
+        eventClick: function(arg) {}
+    });
+
+    calendar.render();
+});
