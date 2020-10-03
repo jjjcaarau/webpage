0d119cfad58bdc7bd232b431eebc7374989300d3
Noah Hüsser <yatekii@yatekii.ch>
2020-10-03 18:27:28 +0200

    Fix Mail delivery of attachements according to the correct RFC

--- backend/src/tera_engine.rs
+++ backend/src/tera_engine.rs
@@ -1,6 +1,6 @@
 lazy_static! {
     pub static ref TERA: tera::Tera = {
-        let tera = tera::compile_templates!("templates/generic/**/*.tera");
+        let tera = tera::Tera::new("templates/generic/**/*.tera").unwrap();
         tera
     };
 }
